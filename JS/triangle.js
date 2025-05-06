0ebed655d79e0066ca82784241ca972a6e719954
ArghoSaha1010 <arghosaha1010@gmail.com>
2025-05-06 19:56:37 +0600

    fixed the btn of Ellipse

--- JS/triangle.js
+++ JS/triangle.js
@@ -11,7 +11,12 @@ document.getElementById("triangleCalculateBtn").addEventListener("click", functi
         document.getElementById("answerOfTriangle").innerText = rectangleArea.toFixed(2);
         document.getElementById("trivalidAlert").style.display = "none";
         document.getElementById("triangleOutput").style.display = "block";
-    } else {
+        document.getElementById("triangleInputB").style.outline = "none";
+        document.getElementById("triangleInputH").style.outline = "none";
+    } else { 
+        document.getElementById("triangleInputB").style.outline = "2px solid red";
+        document.getElementById("triangleInputH").style.outline = "2px solid red";
+        
         document.getElementById("trivalidAlert").innerText = "Please Enter a Valid Number!";
         document.getElementById("trivalidAlert").style.display = "block";
         document.getElementById("triangleOutput").style.display = "none";
